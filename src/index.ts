a4bd2fce8279d3c172e11040d9153738426370cc
David Fant <david@fant.io>
2020-08-26 00:44:17 +0200

    useImmediateLayoutAnimation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { LayoutAnimation, LayoutAnimationConfig } from 'react-native';
 
+function usePrevious<T>(val: T): T {
+    const prev = useRef<T>();
+    useEffect(() => {
+        prev.current = val;
+    });
+    return prev.current;
+}
+
 export function useLayoutAnimation(
-    triggers: ReadonlyArray<any>,
+    deps: ReadonlyArray<any>,
     config: LayoutAnimationConfig = LayoutAnimation.Presets.easeInEaseOut,
     onAnimationDidEnd?: () => void,
 ) {
     useEffect(
         // Note: when using this with eg react-native-web,
         // we need to make sure that LayoutAnimation is set
-        () => !!LayoutAnimation && LayoutAnimation.configureNext(config, onAnimationDidEnd),
-        triggers,
+        () => LayoutAnimation?.configureNext(config, onAnimationDidEnd),
+        deps,
     );
+}
+
+export function useImmediateLayoutAnimation(
+    deps: ReadonlyArray<any>,
+    config: LayoutAnimationConfig = LayoutAnimation.Presets.easeInEaseOut,
+    onAnimationDidEnd?: () => void,
+) {
+    const prevDeps = usePrevious(deps);
+    const hasChanged = !!prevDeps && deps.some((dep, index) => dep !== prevDeps[index]);
+    if (hasChanged) {
+        LayoutAnimation?.configureNext(config, onAnimationDidEnd)
+    }
 }
